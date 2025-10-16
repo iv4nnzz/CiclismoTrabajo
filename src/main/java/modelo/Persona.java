b10c8d52ad05fc6ebd665b2824208c2607c53554
ivan <9spotzs@gmail.com>
2025-10-16 10:42:39 -0500

    Getters y Setters agregado a la clase Persona

--- src/main/java/modelo/Persona.java
+++ src/main/java/modelo/Persona.java
@@ -18,4 +18,18 @@ public class Persona {
         this.edad = edad;
         this.pais = pais;
     }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public String getPais() { return pais; }
+    public void setPais(String pais) { this.pais = pais; }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Edad: " + edad + ", País: " + pais;
+    }
 }
