818022fbb86eb68117c750c6be3238da49fab225
ivan <9spotzs@gmail.com>
2025-10-16 10:43:53 -0500

    Hacer la clase Vista

---
+++ src/main/java/vista/Vista.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author 9spot
+ */
+
+
+public class Vista {
+
+}
