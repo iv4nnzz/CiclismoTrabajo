704fb4236e1eeb136158b5a4ebc884b7c552afa5
ivan <9spotzs@gmail.com>
2025-10-16 10:47:57 -0500

    añadir los atributos y el constructor, esta clase hereda persona

--- src/main/java/modelo/Competidor.java
+++ src/main/java/modelo/Competidor.java
@@ -9,5 +9,16 @@ package modelo;
  * @author 9spot
  */
 public class Competidor extends Persona {
-    
+    private int rankingMundial;
+    private double estatura;
+    private double peso;
+    private int puntos; 
+
+    public Competidor(String nombre, int edad, String pais, int rankingMundial, double estatura, double peso) {
+        super(nombre, edad, pais);
+        this.rankingMundial = rankingMundial;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.puntos = 0;
+    }
 }
