5a341c03c00dcd9715bfef1c2be9d0d220d916dd
ivan <9spotzs@gmail.com>
2025-10-16 11:15:11 -0500

    Añadir los atributos y el constructor de Competencia

--- src/main/java/modelo/Competencia.java
+++ src/main/java/modelo/Competencia.java
@@ -8,7 +8,16 @@ package modelo;
  *
  * @author 9spot
  */
+import java.util.ArrayList;
+import java.util.List;
 
 public class Competencia {
-    
+    private String nombreEvento;
+    private List<Equipo> equipos;
+
+    public Competencia(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+        this.equipos = new ArrayList<>();
+    }
+
 }
