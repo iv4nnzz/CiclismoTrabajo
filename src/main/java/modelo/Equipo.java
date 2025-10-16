2829442f59576b8f39d803b4bd71b2c57c077634
ivan <9spotzs@gmail.com>
2025-10-16 10:37:45 -0500

    Crear clase Equipo

---
+++ src/main/java/modelo/Equipo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package modelo;
+
+/**
+ *
+ * @author 9spot
+ */
+
+public class Equipo {
+    
+}
