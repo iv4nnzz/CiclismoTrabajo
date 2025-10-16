11b85867e2ed6bb00d583546122bdebc951cdab5
ivan <9spotzs@gmail.com>
2025-10-16 11:04:37 -0500

    Agregar atributos y el constructor de la clase Equipo

--- src/main/java/modelo/Equipo.java
+++ src/main/java/modelo/Equipo.java
@@ -8,7 +8,18 @@ package modelo;
  *
  * @author 9spot
  */
+import java.util.ArrayList;
+import java.util.List;
 
 public class Equipo {
-    
+    private String nombre;
+    private String pais;
+    private List<Competidor> competidores;
+
+    public Equipo(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.competidores = new ArrayList<>();
+    }
+
 }
