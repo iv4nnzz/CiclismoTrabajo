09b5ae693c41383b488401cc1d74af15bd3043db
ivan <9spotzs@gmail.com>
2025-10-16 11:08:33 -0500

    Agregar getters y setters, y agregar competidor (sobrecarga) y sobreccarga de crear y agregar competidor directamente

--- src/main/java/modelo/Equipo.java
+++ src/main/java/modelo/Equipo.java
@@ -22,4 +22,21 @@ public class Equipo {
         this.competidores = new ArrayList<>();
     }
 
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getPais() { return pais; }
+    public void setPais(String pais) { this.pais = pais; }
+
+    public List<Competidor> getCompetidores() { return competidores; }
+
+    public void agregarCompetidor(Competidor c) {
+        competidores.add(c);
+    }
+
+    public void agregarCompetidor(String nombre, int edad, String pais, int ranking, double estatura, double peso) {
+        Competidor c = new Competidor(nombre, edad, pais, ranking, estatura, peso);
+        competidores.add(c);
+    }
+
 }
