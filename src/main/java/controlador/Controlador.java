4127d4c47464924b5b8952f62c01f3e42141840c
ivan <9spotzs@gmail.com>
2025-10-16 11:32:13 -0500

    Agregamos los atributos, el constructor y Usamos IF encadenados (if opcion == 1, if opcion == 2, etc)

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -8,7 +8,40 @@ package controlador;
  *
  * @author 9spot
  */
+import modelo.*;
+import vista.Vista;
 
 public class Controlador {
+    private Competencia competencia;
+    private Vista vista;
 
+    public Controlador(Competencia competencia, Vista vista) {
+        this.competencia = competencia;
+        this.vista = vista;
+    }
+
+    public void iniciar() {
+        boolean seguir = true;
+        while (seguir) {
+            int opcion = vista.mostrarMenu();
+
+            if (opcion == 1) {
+                manejarAgregarEquipo();
+            } else if (opcion == 2) {
+                manejarAgregarCompetidor();
+            } else if (opcion == 3) {
+                manejarActualizarPuntos();
+            } else if (opcion == 4) {
+                manejarFinalizarEvento();
+            } else if (opcion == 5) {
+                manejarGenerarReporte();
+            } else if (opcion == 6) {
+                manejarMostrarCompetidoresEquipo();
+            } else if (opcion == 7) {
+                seguir = false;
+            } else {
+                vista.mostrarMensaje("Opción inválida. Intente de nuevo.");
+            }
+        }
+    }
 }
