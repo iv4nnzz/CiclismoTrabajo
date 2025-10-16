38fb01d3e886da75c5ec75a6637357c2fc2913e2
ivan <9spotzs@gmail.com>
2025-10-16 10:55:58 -0500

    Getters y Setteres, y metodo para actualizar puntos

--- src/main/java/modelo/Competidor.java
+++ src/main/java/modelo/Competidor.java
@@ -21,4 +21,21 @@ public class Competidor extends Persona {
         this.peso = peso;
         this.puntos = 0;
     }
+
+    public int getRankingMundial() { return rankingMundial; }
+    public void setRankingMundial(int rankingMundial) { this.rankingMundial = rankingMundial; }
+
+    public double getEstatura() { return estatura; }
+    public void setEstatura(double estatura) { this.estatura = estatura; }
+
+    public double getPeso() { return peso; }
+    public void setPeso(double peso) { this.peso = peso; }
+
+    public int getPuntos() { return puntos; }
+    public void setPuntos(int puntos) { this.puntos = puntos; }
+
+    public void actualizarPuntos(int puntosObtenidos) {
+        this.puntos += puntosObtenidos;
+    }
+
 }
