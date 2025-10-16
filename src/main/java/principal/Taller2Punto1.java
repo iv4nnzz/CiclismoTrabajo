bb0c2529bd82d8af4102d77f54ac180f44e5d675
ivan <9spotzs@gmail.com>
2025-10-16 10:30:03 -0500

    Crear clase principal

---
+++ src/main/java/principal/Taller2Punto1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author 9spot
+ */
+
+public class Taller2Punto1 {
+    public static void main(String[] args) {
+    }
+}
